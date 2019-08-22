6ffca58c64805301e14e7e09f6f25bc15ed17209
Viktor Dychko <viktor.dychko@coya.com>
2019-08-22 15:52:54 +0200

    Implement custom semigroups and monoids

--- src/main/scala/semigroups/MainSemigroup.scala
+++ src/main/scala/semigroups/MainSemigroup.scala
@@ -14,4 +14,46 @@ object MainSemigroup extends App {
 
   println(result1)
 
+
+  trait BooleanSemigroup extends Semigroup[Boolean] {
+    override def combine(x: Boolean, y: Boolean): Boolean = x && y
+  }
+
+  implicit val booleanAndSemigroup: Semigroup[Boolean] = new BooleanSemigroup {}
+
+  implicit val booleanAndMonoid: Monoid[Boolean] = new Monoid[Boolean] with BooleanSemigroup  {
+    override def empty: Boolean = true
+  }
+
+  val result2 = Semigroup[Boolean].combine(true, true)
+  println("Boolean combine: " + result2)
+
+  val result3 = Monoid[Boolean].combine(false, Monoid[Boolean].empty)
+  println("Boolean monoid combine with empty: " + result3)
+
+
+  case class Cocktail(ingredient: List[String], color: String)
+
+  implicit val cocktailMonoid: Monoid[Cocktail] = new Monoid[Cocktail] {
+    override def empty: Cocktail = Cocktail(ingredient = List.empty, color = "transparent")
+
+    override def combine(x: Cocktail, y: Cocktail): Cocktail = {
+      Cocktail(
+        ingredient = x.ingredient |+| y.ingredient,
+        color = y.color
+      )
+    }
+  }
+
+
+  val vodka = Cocktail(ingredient = List("vodka"), color = "transparent")
+  val redBull = Cocktail(ingredient = List("red bull"), color = "pink")
+  val cokeAndRum = Cocktail(ingredient = List("coke", "rum"), color = "brown")
+
+  val fancyCocktail1 = (vodka |+| redBull)|+| cokeAndRum
+  val fancyCocktail2 = vodka |+|(redBull|+| cokeAndRum)
+
+  println(fancyCocktail1)
+  println(fancyCocktail2)
+
 }
